e24e55b3867deb7fe93f06311933df31dbecaabf
Nestor Otondo <nestor.otondo@gmail.com>
2017-07-26 02:00:07 -0400

    test schemas

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+var express = require('express');
+var app     = express();
+var parser  = require('body-parser');
+var db      = require('mongoose');
+const usedb = 'mongodb://127.0.0.1:27017/soccer';
+
+var Schema = db.Schema;
+
+var TeamSchema = new Schema({
+  name: String,
+  country: String
+});
+
+var Team = db.model('Team', TeamSchema);
+
+db.connection.openUri(usedb);
+db.connection.once('open', function() {
+  console.log('Connection established');
+  
+  Team.find({}, function(err, docs) {
+    if (docs) {
+      console.log(docs);
+    }
+  });
+})
+  .on('error', function() {
+    console.error();
+  });
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+/*
 var express = require('express'),
     app = express();
 
@@ -24,3 +59,4 @@ db.connection.once('open', function() {
   .on('error', function() {
     console.error();
   });
+*/
